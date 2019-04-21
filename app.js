30cd8fe048dd5bf89aab139818980fb3191c474a
xl0925 <44955881+xl0925@users.noreply.github.com>
2019-04-20 23:29:24 -0400

    Camera-testing-demo

---
+++ app.js
@@ -0,0 +1,34 @@
+// Set constraints for the video stream
+
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+// define constraints
+const cameraView = document.querySelector("#camera--view"),
+      cameraOutput = document.querySelector("#camera--output"),
+      cameraSensor = document.querySelector("#camera--sensor"),
+      cameraTrigger = document.querySelector("#camera--trigger")
+
+
+// Take a picture when camera Trigger is tapped
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+
+
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+// start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
